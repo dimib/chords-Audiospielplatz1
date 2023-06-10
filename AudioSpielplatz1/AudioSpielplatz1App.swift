018f8ca3b17ab0fa9390ea788cd3e7b0b93e66e8
Dimitri Brukakis <dimitri@brukakis.net>
2023-06-10 10:17:29 +0200

    Initial Commit

---
+++ AudioSpielplatz1/AudioSpielplatz1App.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSpielplatz1App.swift
+//  AudioSpielplatz1
+//
+//  Created by Dimitri Brukakis on 10.06.23.
+//
+
+import SwiftUI
+
+@main
+struct AudioSpielplatz1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioSpielplatz1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AudioSpielplatz1
+//
+//  Created by Dimitri Brukakis on 10.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
